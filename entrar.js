6c5bdea0b9546710f98375c0667a234beae9af83
vini <v9.fantinel@gmail.com>
2025-11-28 19:00:47 -0300

    resolvendo bugs

---
+++ entrar.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+
+function togglePassword(toggleId, inputId) {
+  const toggle = document.getElementById(toggleId);
+  const input = document.getElementById(inputId);
+  toggle.addEventListener("click", () => {
+    if (input.type === "password") {
+      input.type = "text";
+      toggle.src = "hide.png";
+    } else {
+      input.type = "password";
+      toggle.src = "eye.png";
+    }
+  });
+}
+togglePassword("togglePassword", "senha");
