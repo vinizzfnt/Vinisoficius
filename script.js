ac316b7537b7b0b9fce36d02ccf0828b9e00ce25
vini <v9.fantinel@gmail.com>
2025-11-28 17:06:32 -0300

    resolvendo bugs

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const senha = document.getElementById('senha');
 const toggle = document.getElementById('togglePassword');
 
+toggle.src = 'eye.png';
+
 toggle.addEventListener('click', () => {
     if (senha.type === 'password') {
         senha.type = 'text';
