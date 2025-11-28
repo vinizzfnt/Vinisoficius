8ea18657bce7eea6aeb33b7ddaf207a10741baa9
vini <v9.fantinel@gmail.com>
2025-11-28 19:46:20 -0300

    resolvendo bugs

--- cadstro.js
+++ cadstro.js
@@ -1,32 +1,32 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const senha1 = document.getElementById('senha1').value;
-    const senha2 = document.getElementById('senha2').value;
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const senha1 = document.getElementById("senha1").value;
+  const senha2 = document.getElementById("senha2").value;
 
-    if(senha1 !== senha2){
-        alert("As senhas não coincidem!");
-        return;
-    }
+  if (senha1 !== senha2) {
+    alert("As senhas não coincidem!");
+    return;
+  }
 
-    alert("Cadastro realizado com sucesso!");
+  alert("Cadastro realizado com sucesso!");
 });
 
 function togglePassword(toggleId, inputId) {
-    const toggle = document.getElementById(toggleId);
-    const input = document.getElementById(inputId);
+  const toggle = document.getElementById(toggleId);
+  const input = document.getElementById(inputId);
 
-    toggle.addEventListener('click', () => {
-        if (input.type === 'password') {
-            input.type = 'text';
-            toggle.src = 'hide.png';
-        } else {
-            input.type = 'password';
-            toggle.src = 'eye.png';
-        }
-    });
+  toggle.addEventListener("click", () => {
+    if (input.type === "password") {
+      input.type = "text";
+      toggle.src = "hide.png";
+    } else {
+      input.type = "password";
+      toggle.src = "eye.png";
+    }
+  });
 }
 
-togglePassword('toggleSenha1', 'senha1');
-togglePassword('toggleSenha2', 'senha2');
+togglePassword("toggleSenha1", "senha1");
+togglePassword("toggleSenha2", "senha2");
